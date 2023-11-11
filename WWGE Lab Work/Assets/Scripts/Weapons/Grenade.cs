6edec954c93330a2b11798949079814058527435
WereTRex <79789663+WereTRex@users.noreply.github.com>
2023-11-11 17:33:24 +0000

    Grenade Damage
    New Features:
    - Grenade Damage. I have added the ability for grenades to deal damage based on the target's distance to the explosion origin.

--- WWGE Lab Work/Assets/Scripts/Weapons/Grenade.cs
+++ WWGE Lab Work/Assets/Scripts/Weapons/Grenade.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
+    [Tooltip("A damage curve representing the grenade's damage from directly beside the grenade (t = 0s) to the max range (t = 1s)")]
+        [SerializeField] private AnimationCurve _damageCurve;
+    
+    [Space(5)]
+    
     [SerializeField] private float _explosionRadius = 3f;
     [SerializeField] private float _explosionForce = 500f;
     [SerializeField] private float _fuseTime = 2f;
@@ -29,12 +34,16 @@ public class Grenade : MonoBehaviour
     void Explode()
     {
         Destroy(Instantiate(_explosionPrefab, transform.position, Quaternion.identity), _explosionLifetime);
-        
+
         foreach (Collider hitObject in Physics.OverlapSphere(transform.position, _explosionRadius))
         {
             if (hitObject.TryGetComponent<Rigidbody>(out Rigidbody rb))
-            {
                 rb.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, 0.1f);
+
+            if (hitObject.TryGetComponent<HealthComponent>(out HealthComponent healthComponent))
+            {
+                float distance = Vector3.Distance(transform.position, hitObject.ClosestPoint(transform.position));
+                healthComponent.TakeDamage(_damageCurve.Evaluate(distance / _explosionRadius));
             }
         }
 
